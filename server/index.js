75f54fc8b5cacf2c3a78c8286f766e2ad23d3f18
Nicholas Phillips <phillipssnicholas@gmail.com>
2022-02-24 12:13:08 -0700

    rollbar commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const app = express()
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '669e99269abd40efb0ad6f37abe1cd19',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4545
 
 app.listen(port, ()=> console.log(`Running on port ${port}`))
